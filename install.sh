89e390456284c7f395d0ede2c970f4e12ad67841
Aodhan Dalton <aodhan.dalton@rewardinsight.com>
2019-09-02 10:39:37 +0100

    pugin conf edited and install script updated

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 set -e
 
 cd ~/.vim-setup
-cat ~/.vim-setup/vimconigs/0.1.vim > ~/.vimrc
 
+echo 'set runtimepath+=~/.vim-setup
+source ~/.vim-setup/vimconfigs/0.1.vim
+source ~/.vim-setup/vimconfigs/filetypes.vim
+source ~/.vim-setup/vimconfigs/plugin_conf.vim' > ~/.vimrc
 
 echo "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Success ✧ﾟ･: *ヽ(◕ヮ◕ヽ)"
